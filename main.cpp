f7d0cc27bde012bdb51d0aac44ca8fdd03a41175
suleymanovdev <97979588+suleymanovdev@users.noreply.github.com>
2022-07-04 01:13:49 +0400

    MAIN

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <string>
+#include "adtf/classes.h"
+#include "adtf/func.h"
+#include "adtf/menus.h"
 using namespace std;
 
 int main()
 {
-    string system_name;
     
     return 0;
 } 
