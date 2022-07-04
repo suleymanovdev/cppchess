3a98d737900081ae2c7e10475b5992196ca35525
suleymanovdev <97979588+suleymanovdev@users.noreply.github.com>
2022-07-04 22:31:51 +0400

    MAIN FILE

--- main.cpp
+++ main.cpp
@@ -7,6 +7,60 @@ using namespace std;
 
 int main()
 {
-    
+	/*
+		Settings.
+	*/
+
+	int user_arr_size = 0;
+	User* user_arr = new User[user_arr_size];
+	int choose = 0;
+	string sys_name;
+
+	cout << "Enter system name:~> " << endl;
+	cin >> sys_name;
+	clear(sys_name);
+
+    while (true)
+	{
+		clear(sys_name);
+		banner();
+		welcome();
+		cin >> choose;
+		switch (choose)
+		{
+		case 1:
+			clear(sys_name);
+			banner();
+			reg_and_login();
+			break;
+		case 2:
+			clear(sys_name);
+			banner();
+			boardinfo();
+			press_to_continue();
+			break;
+		case 3:
+			clear(sys_name);
+			banner();
+			about();
+			press_to_continue();
+			break;
+		case 4:
+			clear(sys_name);
+			banner();
+			license();
+			press_to_continue();
+			break;
+		case 5:
+			clear(sys_name);
+			banner();
+			cout << "Good bye! Have a nice day!" << endl;
+			exit(0);
+			break;
+		default:
+			break;
+		}
+	}
+	
     return 0;
 } 
