458dac0b850a70f05aa432bcf84199f703e091c5
Elvin Suleymanov <97979588+suleymanovdev@users.noreply.github.com>
2022-07-07 20:38:22 +0400

    ADDITIONAL FILES

--- adtf/func.h
+++ adtf/func.h
@@ -18,52 +18,52 @@ void clear(string system_name)
     }
 }
 
-void addNewUser(User*& arr, int& size, string username, string password, int level)
-{
-	/*
-		1. Creating temp dinamic array and new user.
-		2. Setting new characters for user.
-		3. Sending old users to new array.
-		4. Adding new user to new array.
-	*/
+// void addNewUser(User*& arr, int& size, string username, string password, int level)
+// {
+// 	/*
+// 		1. Creating temp dinamic array and new user.
+// 		2. Setting new characters for user.
+// 		3. Sending old users to new array.
+// 		4. Adding new user to new array.
+// 	*/
 
-	User* new_user_arr_temp = new User[size + 1];
-	User NewUser;
+// 	User* new_user_arr_temp = new User[size + 1];
+// 	User NewUser;
 
-	NewUser.set_username(username); 
-	NewUser.set_password(password);
-	NewUser.set_level(level);
+// 	NewUser.set_username(username); 
+// 	NewUser.set_password(password);
+// 	NewUser.set_level(level);
 
-	for (int i = 0; i < size; i++)
-	{
-		new_user_arr_temp[i] = arr[i];
-	}
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		new_user_arr_temp[i] = arr[i];
+// 	}
 
-	new_user_arr_temp[size] = NewUser;
-	size++;
+// 	new_user_arr_temp[size] = NewUser;
+// 	size++;
 
-	delete[] arr;
-	arr = new_user_arr_temp;
-}
+// 	delete[] arr;
+// 	arr = new_user_arr_temp;
+// }
 
-int findUserIndex(User*& arr, int& size, string username, string password, int level)
-{
-	/*
-		Searching user index by:
-		1. Username.
-		2. Password.
-		3. Level (chess).
-	*/
+// int findUserIndex(User*& arr, int& size, string username, string password, int level)
+// {
+// 	/*
+// 		Searching user index by:
+// 		1. Username.
+// 		2. Password.
+// 		3. Level (chess).
+// 	*/
 
-	for (int i = 0; i < size; i++)
-	{
-		if (arr[i].get_username() == username || arr[i].get_password() == password || arr[i].get_level() == level)
-		{
-			return i;
-		}
-	}
-	return -1;
-}
+// 	for (int i = 0; i < size; i++)
+// 	{
+// 		if (arr[i].get_username() == username || arr[i].get_password() == password || arr[i].get_level() == level)
+// 		{
+// 			return i;
+// 		}
+// 	}
+// 	return -1;
+// }
 
 void press_to_continue()
 {
