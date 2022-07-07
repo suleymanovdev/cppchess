28c223d5557ad41d9bd3a55ce2072ee15e0b9b21
Elvin Suleymanov <97979588+suleymanovdev@users.noreply.github.com>
2022-07-07 20:41:05 +0400

    MAIN FILE

--- main.cpp
+++ main.cpp
@@ -7,82 +7,105 @@ using namespace std;
 
 int main()
 {
-	/*
-		Settings.
-	*/
+    /*
+      User settings.
+    */
+  
+    User user;
+    string username;
+    string password;
+    int level;
 
-	int user_arr_size = 0;
-	User* user_arr = new User[user_arr_size];
-	int choose = 0;
-	string sys_name;
+    /*
+  		Settings.
+  	*/
+  
+  	int choose = 0;
+  	string sys_name;
 
-	cout << "Enter system name:~> ";
-	cin >> sys_name;
-	clear(sys_name);
+  	cout << "Enter system name:~> ";
+  	cin >> sys_name;
+  	clear(sys_name);
 
     while (true)
 	{
-		clear(sys_name);
-		banner();
-		welcome();
-		cout << endl;
-		cout << "	Enter your choose:~> ";
-		cin >> choose;
-		switch (choose)
+  		clear(sys_name);
+  		banner();
+  		welcome();
+  		cout << endl;
+  		cout << "	Enter your choose(menu):~> ";
+  		cin >> choose;
+		if (choose == 1)
 		{
-		case 1:
 			int choose_reg;
 			clear(sys_name);
 			banner();
 			reg_and_login();
-			/*
-                    1. Registration.
-					2. Log In.
-					3. Log In as Guest. (Beta)
-					4. Back to menu.
-			*/
-			// cout << "	Enter your choose:~> ";
-			// cin >> choose_reg;
-			// switch (choose_reg)
-			// {
-			// case 1:
-			// 	string username = "sudev";
-			// 	string password = "passtest45";
-			// 	int level = 1045;
-			// 	addNewUser(user_arr, user_arr_size, username, password, level);
-			// 	cout << user_arr[1].get_username();
-			// 	press_to_continue();
-			// 	break;
-			// }
-			break;
-		case 2:
+			cout << " Enter your choose(reg):~> ";
+			cin >> choose_reg;
+			if (choose_reg == 1)
+			{
+				cout << " Enter your name:~> ";
+				cin >> username;
+				cout << " Enter your pass:~> ";
+				cin >> password;
+				cout << " Enter your level:~> ";
+				cin >> level;
+
+				user.set_username(username);
+				user.set_password(password);
+				user.set_level(level);
+
+				user.inf0();
+
+				press_to_continue();
+			}
+			else if (choose_reg == 2)
+			{
+				continue;
+			}
+			else if (choose_reg == 3)
+			{
+				continue;
+			}
+			else
+			{
+				cout << "Err: Reg (!)" << endl;
+			}
+		}
+		else if (choose == 2)
+		{
 			clear(sys_name);
 			banner();
 			boardinfo();
 			press_to_continue();
-			break;
-		case 3:
+		}
+		else if (choose == 3)
+		{
 			clear(sys_name);
 			banner();
 			about();
 			press_to_continue();
-			break;
-		case 4:
+		}
+		else if (choose == 4)
+		{
 			clear(sys_name);
 			banner();
 			license();
 			press_to_continue();
-			break;
-		case 5:
+		}
+		else if (choose == 5)
+		{
 			clear(sys_name);
 			banner();
 			cout << "Good bye! Have a nice day!" << endl;
 			exit(0);
-			break;
-		default:
-			break;
+		}
+		else 
+		{
+			cout << "Err: Main (!)" << endl;
 		}
 	}
-	
+  
     return 0;
 } 
