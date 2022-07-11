0e9528af0a9aa4e538f8c3127b789796883419bd
Elvin Suleymanov <97979588+suleymanovdev@users.noreply.github.com>
2022-07-11 19:57:40 +0000

    MAIN FILE

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string>
+#include "additional_files/classes.h"
+#include "additional_files/menus.h"
+#include "additional_files/functions.h"
 using namespace std;
+
+int main()
+{
+
+  return 0;
+}
