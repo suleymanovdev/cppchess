62f98cef4ca0d2d9f78475c8fd0742385a0a1976
Elvin Suleymanov <97979588+suleymanovdev@users.noreply.github.com>
2022-07-12 15:17:37 +0000

    MAIN FILE

--- main.cpp
+++ main.cpp
@@ -7,7 +7,33 @@ using namespace std;
 
 int main()
 {
+    // Creating arrays.
+    // 1. Board with figures.
+    // 2. White killed figures.
+    // 3. Black killed figures.
+    
+    Figure* board_arr[8][8] = {
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+        {},
+    };
+
+    Figure killed_white_figures_arr[15]{};
+    Figure killed_black_figures_arr[15]{};
+
+    // Creating user.
     User user;
+
+    // Circle.
+    while (true)
+    {
+        
+    }
     
     return 0;
 }
