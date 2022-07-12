3481b22d8aa919a79ff9421d60b6d9aa81e40f18
Elvin Suleymanov <97979588+suleymanovdev@users.noreply.github.com>
2022-07-12 16:02:40 +0000

    MAIN FILE

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace std;
 
 int main()
 {
+    string sys; // Your System Name.
+    
     // Creating arrays.
     // 1. Board with figures.
     // 2. White killed figures.
@@ -23,16 +25,58 @@ int main()
         {},
     };
 
-    Figure killed_white_figures_arr[15]{};
-    Figure killed_black_figures_arr[15]{};
+    Figure* killed_white_figures_arr[15] = {};
+    Figure* killed_black_figures_arr[15] = {};
 
     // Creating user.
     User user;
 
-    // Circle.
+    // Data for cycle.
+    int welcome_choose = 0;
+
+    // Entering system name.
+    cout << "Ou. Hello! Enter your system name:> ";
+    cin >> sys;
+    
+    // Cycle.
     while (true)
     {
-        
+        clear(sys);
+        banner();
+        welcome();
+        cout << "    choose(welcome):> ";
+        cin >> welcome_choose;
+        if (welcome_choose == 1)
+        {
+            clear(sys);
+            banner();
+            reg_and_login();
+            press_to_continue();
+        }
+        else if (welcome_choose == 2)
+        {
+            boardinfo();
+            press_to_continue();
+        }
+        else if (welcome_choose == 3)
+        {
+            
+        }
+        else if (welcome_choose == 4)
+        {
+            
+        }
+        else if (welcome_choose == 5)
+        {
+            
+        }
+        else
+        {
+            clear(sys);
+            cout << "Err: In main() function." << endl;
+            error();
+            exit(0);
+        }
     }
     
     return 0;
